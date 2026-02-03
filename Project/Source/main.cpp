75f9ef11cf754e4255b9b0a6aed903664ae2446d
Alex <alexander.james.swanson@gmail.com>
2026-02-03 05:22:55 -0500

    added Vulkan, Gateware, EnTT, and Imgui to the project

--- Project/Source/main.cpp
+++ Project/Source/main.cpp
@@ -1,6 +1,16 @@
 #include <windows.h>
 #include <cstdio>
+#include <chrono>
+#include <entt/entt.hpp>
+
 int main()
 {
     printf("Hello World!\n");
+
+    // Seed the rand
+    unsigned int time = std::chrono::steady_clock::now().time_since_epoch().count();
+    srand(time);
+
+    // store everything related to entities and components in a single registry
+    entt::registry registry;
 }
